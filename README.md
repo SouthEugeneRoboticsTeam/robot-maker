c18ed3146018406272c698356fff59fe73f32f2f
Andrew Dassonville <dassonville.andrew@gmail.com>
2017-09-17 19:38:27 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Robot Maker
+
+Robot Maker is a tiny command line utility that makes creating yearly
+game repositories super easy. It bases the new code off of the
+[Robot-Base](https://github.com/SouthEugeneRoboticsTeam/Robot-Base)
+repository and updates everything that needs to be changed. All you
+have to do is push the code!
+
+_Only tested on macOS at the moment._
+
+## Usage
+
+```bash
+./robot-maker <path_to_robot> <game_name>
+```
+
+For example...
+
+```bash
+./robot-maker ../PowerUp-2018 PowerUp
+```
+
+This command will create a new repository with all the robot code
+you'll need to get started.
---
+++ robot-maker.sh
@@ -0,0 +1,23 @@
+BASE_ROBOT_REPO=https://github.com/SouthEugeneRoboticsTeam/Robot-Base
+ROBOT_PATH=$1
+GAME_NAME=$(echo $2 | tr "[:upper:]" "[:lower:]") 
+
+git clone -q --depth=1 $BASE_ROBOT_REPO $ROBOT_PATH
+
+cd $ROBOT_PATH
+
+rm -rf .git
+
+sed -i "" "s/org\.sert2521\.gamename\.Robot/org\.sert2521\.$GAME_NAME\.Robot/g" build.gradle
+
+find src -type f -print0 | xargs -0 sed -i "" "s/org\.sert2521\.gamename/org\.sert2521\.$GAME_NAME/"
+
+mv src/main/java/org/sert2521/gamename src/main/java/org/sert2521/$GAME_NAME
+mv src/test/java/org/sert2521/gamename src/test/java/org/sert2521/$GAME_NAME
+
+git init -q
+git add . > /dev/null
+git commit -q -m "Initial commit"
+
+echo "Robot creation complete!"
+echo "Use \`git remote add origin <repo_url>\`, then push to GitHub."
