b37702fdeb6952f91df72bfdaf84f601a8211541
Andrew Dassonville <dassonville.andrew@gmail.com>
2018-01-23 16:55:01 -0800

    Fix README generation
    Signed-off-by: Andrew Dassonville <dassonville.andrew@gmail.com>

--- robot-maker.sh
+++ robot-maker.sh
@@ -47,10 +47,10 @@ mv src/main/java/org/sert2521/gamename src/main/java/org/sert2521/$GAME_NAME_SM
 
 # Remove README and create new one from README_TEMPLATE
 rm README.md
-mv README_TEMPLATE.md README.md
-sed -i "" "s/{{name}}/$GAME_NAME/g" README.md
-sed -i "" "s/{{name_full}}/$GAME_NAME_FULL/g" README.md
-sed -i "" "s/{{year}}/$YEAR/g" README.md
+cat README_TEMPLATE.md | sed "s/{{name}}/$GAME_NAME/g" | \
+                         sed "s/{{name_full}}/$GAME_NAME_FULL/g" | \
+                         sed "s/{{year}}/$YEAR/g" > README.md
+rm README_TEMPLATE.md
 
 git init -q
 
